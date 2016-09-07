f5512d3d0c3eee83c1700e0e80515efd02f0ffe6
tophf <tophf@gmx.com>
2016-09-07 14:08:07 +0300

    code cosmetics, more categories, more ES6, fixups

--- background.js
+++ background.js
@@ -1,28 +1,45 @@
-const MAX_CACHE_AGE = 24 * 3600 * 1000; // ms
+const MAX_CACHE_AGE = 7 * 24 * 3600 * 1000; // ms, 7 days
 const REQUEST_DELAY = 200; // ms
 
-const API_URL = 'https://myanimelist.net/search/prefix.json?type=%t&v=1&keyword=';
-const SEARCH_URL = 'https://myanimelist.net/anime.php?q=';
 const MAL_URL = 'https://myanimelist.net/';
+const API_URL = MAL_URL + 'search/prefix.json?type=%t&v=1&keyword=';
+const SEARCH_URL = MAL_URL + 'anime.php?q=';
 
 const STORAGE_QUOTA = 5242880;
-const CATEGORIES = {a: 'anime', m: 'manga', c: 'character', p: 'person', '': 'all'};
+const CATEGORIES = {
+	a: 'anime',
+	m: 'manga',
+	c: 'character',
+	p: 'person',
+	u: 'user',
+	n: 'news',
+	f: 'forum',
+	k: 'club',
+	'': 'all'
+};
+const CATEGORY_SPLITTER = new RegExp('^(.*?)(?:/([' +
+	Object.keys(CATEGORIES).join('') + ']))?$', 'i');
 
 let g = {
-	category: '',
-	text: '',
+	text: '',          // sanitized
+	textForCache: '',  // sanitized with optional '/' + category 1-letter key
+	textForURL: '',    // sanitized %-encoded
+	category: '',      // full name of category specified after /
+	siteLink: '',      // top suggestion with site search link and info
 
-	xhrScheduled: 0,
+	xhrScheduled: 0,   // setTimeout id
 	xhr: new XMLHttpRequest(),
 
 	cache: chrome.storage.local,
 
-	img: document.body.appendChild(document.createElement('img')),
-	canvas: document.body.appendChild(document.createElement('canvas')),
+	img: document.createElement('img'),
+	canvas: document.createElement('canvas'),
 	canvas2d: null,
+	best: null,        // the best match to display in a notification
 };
 g.xhr.responseType = 'json';
 g.canvas2d = g.canvas.getContext('2d');
+g.img.onload = showImageNotification;
 
 /*****************************************************************************/
 
@@ -33,7 +50,8 @@ chrome.omnibox.onInputChanged.addListener(onInputChanged);
 chrome.omnibox.onInputEntered.addListener(text =>
 	chrome.tabs.update({
 		url: text.match(/^https?:/) ? text
-		                            : text.trim() ? SEARCH_URL + g.textForURL : MAL_URL
+		                            : text.trim() ? SEARCH_URL + g.textForURL
+		                                          : MAL_URL
 	}));
 
 chrome.omnibox.onInputCancelled.addListener(abortRequest);
@@ -45,33 +63,30 @@ chrome.alarms.onAlarm.addListener(alarm =>
 
 function onInputChanged(text, suggest)
 {
-	g.category = CATEGORIES[(text.match(/\/([amcp])$/) || {1:''})[1]];
-	g.text = normalizeSpaces(text).replace(/[!-\/:-?\[-`{-~\s]+$/, '');
-	g.textForURL = encodeURIComponent(g.text.replace(/\/([amcp])$/, ''));
-	g.siteLink = `<dim>Search for <match>${escapeXML(g.text)}</match> on site.</dim>`;
+	var m = text.trim().match(CATEGORY_SPLITTER);
+	g.category = CATEGORIES[m[2] || ''];
+	g.text = sanitizeInput(m[1]);
+	g.textForURL = encodeURIComponent(m[1]);
+	g.textForCache = 'input:' + g.text + (m[2] ? '/'+m[2] : '');
 
+	g.siteLink = `<dim>Search for <match>${escapeXML(g.text)}</match> on site.</dim>`;
 	chrome.omnibox.setDefaultSuggestion({description: g.siteLink});
 
-	if (text.length < 3)
+	if (g.text.length < 3)
 		return;
 
-	pipe(
+	pipeAsync(
 		readCache,
 		searchMAL,
 		updateCache,
-		data => {
-			chrome.omnibox.setDefaultSuggestion({description: data.siteLink});
-			suggest(data.suggestions);
-			notify(data.best);
-		}
+		data => displayData(data, suggest)
 	);
 }
 
 function readCache(data, next)
 {
-	var key = 'input:' + g.text;
-	g.cache.get(key, results =>
-		next(results[key])
+	g.cache.get(g.textForCache, results =>
+		next(results[g.textForCache])
 	);
 }
 
@@ -84,43 +99,58 @@ function searchMAL(data, next)
 
 	g.xhrScheduled = setTimeout(() => {
 		g.xhr.open('GET', API_URL.replace('%t', g.category) + g.textForURL);
-		g.xhr.onreadystatechange = () =>
-			g.xhr.readyState == XMLHttpRequest.DONE && g.xhr.status == 200
-			? next(g.xhr.response) : null;
+		g.xhr.onreadystatechange = () => {
+			if (g.xhr.readyState == XMLHttpRequest.DONE && g.xhr.status == 200)
+				next(cookSuggestions(g.xhr.response));
+		};
 		g.xhr.send();
 	}, REQUEST_DELAY);
 }
 
 function updateCache(data, next)
 {
-	if (!data)
-		return;
 	if (!data.expires) {
-		data = cookSuggestions(data);
-		g.cache.set({['input:' + g.text]: data});
-		g.cache.getBytesInUse(null, size => size > STORAGE_QUOTA/2 && g.cache.clear());
-		chrome.alarms.create(g.text, {when: data.expires});
+		data.expires = Date.now() + MAX_CACHE_AGE;
+		g.cache.set({[g.textForCache]: data});
+		g.cache.getBytesInUse(null, size => size > STORAGE_QUOTA/2 && cleanupCache());
+		chrome.alarms.create(g.textForCache, {when: data.expires});
 	}
 	next(data);
 }
 
-function notify(best) {
-	if (!best)
+function cleanupCache() {
+	// remove the oldest half of the items
+	g.cache.get(null, data => {
+		var keys = Object.keys(data);
+		keys.sort((a,b) => data[a].expires - data[b].expires);
+		g.cache.remove(keys.slice(0, keys.length/2 |0));
+	});
+}
+
+function displayData(data, suggest) {
+	chrome.omnibox.setDefaultSuggestion({description: data.siteLink});
+	suggest(data.suggestions);
+	if (data.best) {
+		g.img.src = data.best.image;
+		g.best = data.best;
+	}
+}
+
+function showImageNotification(event) {
+	// skip placeholder images
+	if (this.naturalWidth < 50)
 		return;
-	g.img.src = best.image;
-	g.img.onload = (e) => {
-		g.canvas.width = g.img.naturalWidth;
-		g.canvas.height = g.img.naturalHeight;
-		g.canvas2d.drawImage(g.img, 0, 0);
-		chrome.notifications.create('best', {
-			type: 'image',
-			iconUrl: 'icon/128.png',
-			imageUrl: g.canvas.toDataURL(),
-			title: best.name + ' (' + best.type + ')',
-			message: best.dates,
-			contextMessage: best.status,
-		});
-	};
+	g.canvas.width = this.naturalWidth;
+	g.canvas.height = this.naturalHeight;
+	g.canvas2d.drawImage(this, 0, 0);
+	chrome.notifications.create('best', {
+		type: 'image',
+		iconUrl: 'icon/128.png',
+		imageUrl: g.canvas.toDataURL(),
+		title: g.best.name + ' (' + g.best.type + ')',
+		message: g.best.dates,
+		contextMessage: g.best.status,
+	});
 }
 
 function abortRequest()
@@ -131,16 +161,11 @@ function abortRequest()
 
 function cookSuggestions(found)
 {
-	if (!found)
-		return;
-
 	var rxWords = wordsAsRegexp(g.text, 'gi');
 	var best;
 	return {
-		siteLink: g.siteLink + ' Found in categories: ' +
-			found.categories
-				.map(cat => `${cat.type} (${cat.items.length})`)
-				.join(', '),
+		siteLink: g.siteLink + ' Found in categories: ' + found.categories
+			.map(cat => `${cat.type} (${cat.items.length})`).join(', '),
 		suggestions: []
 			.concat.apply([], found.categories.map(cat => cat.items.map(_preprocess)))
 			.sort((a,b) => b.weight - a.weight)
@@ -152,7 +177,6 @@ function cookSuggestions(found)
 			dates: best.payload.aired || best.payload.published || '',
 			image: best.image_url.replace(/\/r\/\d+x\d+|\?.*/g, ''),
 		},
-		expires: Date.now() + MAX_CACHE_AGE,
 	};
 
 	function _preprocess(item) {
@@ -160,15 +184,17 @@ function cookSuggestions(found)
 		var payload = item.payload;
 		var type = item.type = payload.media_type || item.type || '';
 		var name = item.name = item.name.replace(new RegExp(`\\s+${type}$`), '');
+
 		var marked = item.marked = name.replace(rxWords, '\r$&\n');
-		item.weight += 100 * (isInCategory ? 1 : 0) +
-		                10 * (marked.match(/^\r|^/g).length - 1) +
-		                 4 * (marked.match(/ \r|^/g).length - 1) +
-		                     (marked.match(/\S\r|^/g).length - 1);
+		item.weight = 100 * (isInCategory ? 1 : 0) +
+		               10 * (marked.match(/^\r|$/g).length - 1) +
+		                4 * (marked.match(/ \r|$/g).length - 1) +
+		                    (marked.match(/\S\r|$/g).length - 1);
+
+		var status = (payload.status || '').replace(/Finished.*|Currently\s*/, '');
 		var related = (payload.related_works || []).join(', ');
 		var altName = payload.alternative_name;
-		item.status = reescapeXML(payload.status || related || altName || '')
-		             .replace(/Finished.*|Currently\s*/, '');
+		item.status = reescapeXML(status || related || altName || '');
 		return item;
 	}
 
@@ -225,22 +251,21 @@ function reescapeXML(s)
 	return escapeXML(unescapeXML(s));
 }
 
-function normalizeSpaces(s)
+function sanitizeInput(s)
 {
-	// strip leading spaces, replace 2+ spaces with one space (which allows 1 trailing space)
-	return s.replace(/^\s+/, '').replace(/\s{2,}/, ' ');
+	// trim punctuation at start/end, replace 2+ spaces with one space
+	return s.replace(/^[!-\/:-?\[-`{-~\s]+/, '')
+	        .replace(/\s{2,}/, ' ')
+	        .replace(/[!-\/:-?\[-`{-~\s]+$/, '');
 }
 
-function pipe()
+function pipeAsync(...functions)
 {
-	var hasSeed = typeof arguments[0] != 'function';
-	var fnIndex = hasSeed ? 1 : 0;
-	var fns = Array.apply(null, arguments);
-
-	_next(hasSeed ? arguments[0] : undefined);
+	var index = 0;
+	_next(undefined);
 
 	function _next(data) {
-		if (fnIndex < fns.length)
-			fns[fnIndex++](data, _next);
+		if (index < functions.length)
+			functions[index++](data, _next);
 	}
 }
